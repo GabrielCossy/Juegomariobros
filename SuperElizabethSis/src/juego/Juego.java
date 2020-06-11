c3c3a4b4bb87db151db6d6d394b05b1978099085
ovied <ovied@CHARLY_DESK>
2020-06-11 09:26:15 -0300

    Se corrigio modificacion por error del Juego.java

--- SuperElizabethSis/src/juego/Juego.java
+++ SuperElizabethSis/src/juego/Juego.java
@@ -1,17 +1,32 @@
 package juego;
 
 
+
+import java.awt.Image;
 import entorno.Entorno;
+import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
 public class Juego extends InterfaceJuego
 {
+	Princesa princesa;	
+	//asdtrhyt
+	//dsfsdfsdfsdf
+	//sdasdasd
+	public static final String Princesa = null;
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
+	private Obstaculo obstaculo;
+	private Soldado soldado;
+	private Nube nube;
+	
+	
+	public Object x;
 	
 	// Variables y métodos propios de cada grupo
 	// ...
-	
+	//Image fondo;
+	//fondo=Herramientas.cargarImagen("fondo.png");
 	Juego()
 	{
 		// Inicializa el objeto entorno
@@ -19,7 +34,12 @@ public class Juego extends InterfaceJuego
 		
 		// Inicializar lo que haga falta para el juego
 		// ...
-
+		this.princesa=new Princesa(100,480,20,40,0);
+		this.soldado=new Soldado(800,480,20,40,0);
+		this.obstaculo=new Obstaculo(700,480,20,60,0);
+		this.nube=new Nube(200,100,100,60,0);
+		
+		
 		// Inicia el juego!
 		this.entorno.iniciar();
 	}
@@ -34,10 +54,26 @@ public class Juego extends InterfaceJuego
 	{
 		// Procesamiento de un instante de tiempo
 		// ...
+		this.soldado.moverAdelante();
+		this.obstaculo.moverAdelante();
+		this.nube.moverAdelante();
+		
+		
+		this.princesa.dibujarse(this.entorno);
+		this.soldado.dibujarse(entorno);
+		this.obstaculo.dibujarse(entorno);
+		this.nube.dibujarse(entorno);
+		
+		
 		
 
 	}
 	
+
+	private void obstaculo(Entorno entorno2) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
--- SuperElizabethSis/src/juego/Soldado.java
+++ SuperElizabethSis/src/juego/Soldado.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 
 import entorno.Entorno;
 
+
+
 public class Soldado {
 	double x;
 	double y;
