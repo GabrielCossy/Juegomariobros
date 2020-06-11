c80819f7dd6b0c9d8a120fe4c13fd9b297aa5733
ovied <ovied@CHARLY_DESK>
2020-06-11 08:52:08 -0300

    Se agrega clase Nube pasada por Gabriel

---
+++ SuperElizabethSis/src/juego/Nube.java
@@ -0,0 +1,44 @@
+package juego;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Nube {
+	double x;
+	double y;
+	double ancho;
+	double alto;
+	double angulo;
+	Image  img1;
+		
+	
+	
+	Nube(double x,double y,double ancho,double alto,double angulo){
+	    this.x=x;
+		this.y=y;
+		this.ancho=ancho;
+		this.alto=alto;
+		this.angulo=angulo;
+		
+	}
+		
+	public void dibujarse(Entorno entorno) {
+		entorno.dibujarRectangulo(x, y,ancho,alto,angulo,Color.	white);
+	}
+	
+	public void moverAdelante() {
+		this.x -= Math.cos(this.angulo)*1;
+		this.y += Math.sin(this.angulo)*2;
+		
+		if (this.x > 850) {
+			this.x=0;
+		}
+		if (this.x < -50) {
+			this.x=850;
+		}	
+		
+	}
+}
